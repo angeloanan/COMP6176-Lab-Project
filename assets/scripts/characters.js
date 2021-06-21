10cef78c60a483c36d90bcd4330b5096bfeb1ec6
Christopher Angelo <angelo@angeloanan.xyz>
2021-06-21 14:19:56 +0700

    feat: add characters page

---
+++ assets/scripts/characters.js
@@ -0,0 +1,13 @@
+// Typescript would help alot here
+// Just saying...
+
+function revealCharacter(area, character) {
+  console.log(`From ${area}, opening ${character}`);
+  document.querySelectorAll(`#${area}-gallery .gallery-content`).forEach((el) => el.style.display = 'none')
+  document.querySelector(`#${character}`).style.display = 'flex'
+}
+
+window.addEventListener('load', () => {
+  document.getElementById("amber").style.display = 'flex'
+  document.getElementById("ganyu").style.display = 'flex'
+})
